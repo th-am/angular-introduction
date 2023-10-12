27b03bdf12f0e9ac5c781d598b35711d2f1dfac2
th-am <th.amarantos@outlook.com>
2023-10-12 19:23:33 +0300

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string = "Thodoris";
+  lastname = "A."
+}
